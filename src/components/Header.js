f449f6c4c4f9f595dd2469dd02f5e921f89a4bdb
Alex Velazquez <arichard.1190@gmail.com>
2023-01-12 11:33:35 -0300

    Primer commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import "./Header.css";
+
+class Header extends Component {
+    render() {
+        return <section className="header">Header.</section>;
+    }
+}
+    
+export default Header;
